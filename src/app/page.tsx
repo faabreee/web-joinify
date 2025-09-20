11ba0016c40b1be7d46ee2d4569fc90cf52a93b5
faabreee <reyescocafagardo@gmail.com>
2025-09-20 15:24:33 -0500

    feat: implement user provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,31 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { UserContext } from "@/utils/UserProvider";
 import Link from "next/link";
+import { useRouter } from 'next/navigation'
+import { useContext } from "react";
 
 type Props = {
 
 }
 
-export default function Index({
+export default function Index({}: Props) {
+    const userContext = useContext(UserContext);
+    const router = useRouter();
 
-}: Props) {
+    if (!userContext) {
+        return <p>No hay contexto disponible</p>;
+    }
+
+    const { user, setUser } = userContext;
+
+    if (!user) router.push("/authentication/login");
+
+
     return (
         <>
-            <h1>Holaaa</h1>
+            <h1>Holaaa {user?.fullName} </h1>
 
             <Button>Holaaa</Button>
 
---
+++ src/utils/UserProvider.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { createContext, useState } from "react";
+
+// Datos del usuario
+interface IUser {
+    uuid: string;
+    username: string;
+    fullName: string;
+    email: string;
+    token: string;
+  }
+  
+  // Datos del contexto
+interface IUserContext {
+    user: IUser | null;
+    setUser: React.Dispatch<React.SetStateAction<IUser | null>>;
+}
+
+
+export const UserContext = createContext<IUserContext | null>(null);
+
+
+export const UserProvider = ({ children }: { children: React.ReactNode }) => {
+    
+    const [user, setUser] = useState<IUser | null>({
+      uuid: "c9797343-ab0d-4b53-ab79-d5b5bdcd0a4f",
+      username: "freyes",
+      fullName: "Fabrizio Reyes",
+      email: "reyescocafagardo@gmail.com",
+      token: "KG91tmf3cYqBKFcBVXIilsJ7gOeGBprI9qWPXmkYul6eh1uv5yWE8P8IUFY2UfZl",
+    });
+
+    // const [user, setUser] = useState<IUser | null>(null);
+
+    return (
+        <UserContext.Provider value={{ user, setUser }}>
+          {children}
+        </UserContext.Provider>
+      );
+};
